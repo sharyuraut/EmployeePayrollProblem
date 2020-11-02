d3648b82185cfc58ee27a57e5ccb569fbe83aff9
sharyu raut <sharyukraut210298@gmail.com>
2020-11-02 23:56:36 +0530

    [Sharyu] Add. Initial Commit

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source='(LocalDB)\ABC';Initial Catalog=EmployeeDetails;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
