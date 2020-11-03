8b28c3a630e3ff1f9a9890b5072277bd9a0a4993
sharyu raut <sharyukraut210298@gmail.com>
2020-11-03 08:23:39 +0530

    [Sharyu] Add. establishing connection for Employee Payroll

--- EmployeePayrollService/EmployeePayroll.cs
+++ EmployeePayrollService/EmployeePayroll.cs
@@ -11,7 +11,7 @@ namespace EmployeePayrollService
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string Department { get; set; }
-        public char Gender { get; set; }
+        public string Gender { get; set; }
         public double BasicPay { get; set; }
         public double Deductions { get; set; }
         public double TaxablePay { get; set; }
